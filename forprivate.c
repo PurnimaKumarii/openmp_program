0ba70bdf88de7ec8b5536b1089cc849377591d6d
PurnimaKumarii <purnimacse.a@gmail.com>
2024-02-09 15:59:30 +0530

    openmp

---
+++ forprivate.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<omp.h>
+#define N 5
+ int main()
+  {
+    int a=20;
+    int b=10;
+    int c=40;
+    omp_set_num_threads(N);
+#pragma omp parallel for private(c)
+for(int i=0; i<N; i++)
+ {
+   printf("Thread %d value is %d\n",omp_get_thread_num(),c);
+  c=a+b; //private
+  printf("Thread %d value is %d\n",omp_get_thread_num(),c);
+  }
+printf("%d",c);
+return 0;
+}
+
+
